429034449f73ef6083b0706c5aa79a0114c93448
Nicole Sauer <95630400+NicoleSauer@users.noreply.github.com>
2023-10-16 20:57:54 +0200

    added cors

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 # application dependencies
 from flask import Flask
+from flask_cors import CORS
 
 # Imports of the resources
 from resources.demo_resource import demo_resource
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Registers the resources, which are needed.
 # The (Flask-Restful)-Resources in this case are blueprints, which contain the different rest apis
