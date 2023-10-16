ec9cf2b8c5181c0b90da1e22f8fcbe0a2873b8ea
Nicole Sauer <95630400+NicoleSauer@users.noreply.github.com>
2023-10-16 20:56:13 +0200

    fixed spelling

--- src/app.py
+++ src/app.py
@@ -1,13 +1,13 @@
 # application dependencies
 from flask import Flask
 
-# Imports of the ressources
+# Imports of the resources
 from resources.demo_resource import demo_resource
 
 app = Flask(__name__)
 
-# Registers the ressources, which are needed.
-# The (Flask-RestX)-Resources in this case are blueprints, which contain the diffrent rest apis
+# Registers the resources, which are needed.
+# The (Flask-Restful)-Resources in this case are blueprints, which contain the different rest apis
 app.register_blueprint(demo_resource)
 
 
