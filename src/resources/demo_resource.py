1c8f2a47b4097114cef51f48478cee0bda0ee26e
Nicole Sauer <95630400+NicoleSauer@users.noreply.github.com>
2023-10-20 17:22:29 +0200

    changed requirements and added additional info to readme

--- src/resources/demo_resource.py
+++ src/resources/demo_resource.py
@@ -5,12 +5,14 @@ from flask_restful import Api, Resource
 demo_resource = Blueprint('demo_resource', __name__)
 api = Api(demo_resource)
 
+
 # Create a new resource
 class DemoRestResource(Resource):
-    
+
     # Create a get request for the resource
     def get(self):
         return 'Hello World!'
+
 
 # Add the resource to the api
 api.add_resource(DemoRestResource, '/demo')
