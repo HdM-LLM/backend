64edbfc0e41e8861a86dfc6c9f6966388a59f98f
LiamWilke <lw110@hdm-stuttgart.de>
2023-11-20 13:41:54 +0100

    evaluation of applicants without VDB & Database changes

--- src/db/sql-scripts/tables_and_columns.sql
+++ src/db/sql-scripts/tables_and_columns.sql
@@ -7,5 +7,110 @@ CREATE TABLE users (
     LastName VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE job_postings (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    position VARCHAR(255) NOT NULL,
+    description TEXT,
+    creation_date DATE,
+    start_date DATE,
+    department VARCHAR(100),
+    applicant VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE applicants (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+    email_address VARCHAR(255) NOT NULL,
+    position_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (position_id) REFERENCES job_postings(id)
+);
+
+
+CREATE TABLE applicant_ratings (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    applicant_id INT NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    score INT NOT NULL,
+    justification TEXT,
+    quote TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (applicant_id) REFERENCES applicants(id)
+);
+
+
+CREATE TABLE job_categories (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(100) NOT NULL,
+    guideline_0 TEXT,
+    guideline_10 TEXT
+);
+
+CREATE TABLE job_posting_categories (
+    job_posting_id INT NOT NULL,
+    category_id INT NOT NULL,
+    PRIMARY KEY (job_posting_id, category_id),
+    FOREIGN KEY (job_posting_id) REFERENCES job_postings(id),
+    FOREIGN KEY (category_id) REFERENCES job_categories(id)
+);
+
 INSERT INTO users (FirstName, LastName)
 VALUES ('Max', 'Mustermann'), ('Lisa', 'Musterfrau');
+
+INSERT INTO job_postings (
+    position,
+    description,
+    creation_date,
+    start_date,
+    department,
+    applicant
+) VALUES (
+    'java backend developer',
+    'Description of the position...',
+    '2023-01-01',
+    '2023-02-01',
+    'IT Department',
+    'John Doe'
+), (
+    '(Junior) IT System Engineer specializing in IT Networking (w/m/d)',
+    'Description of the position...',
+    '2023-02-01',
+    '2023-03-01',
+    'Networking Department',
+    'Jane Smith'
+);
+
+INSERT INTO job_categories (
+    category_name,
+    guideline_0,
+    guideline_10
+) VALUES (
+    'Coding Skills',
+    'Does not mention any coding-related experiences or skills.',
+    'Highlights extensive coding experiences, showcasing expertise in languages like Python and Java.'
+), (
+    'Communication Skills',
+    'Lacks examples of clear and concise communication in professional contexts.',
+    'Provides multiple instances of clear and effective communication in professional contexts.'
+), (
+    'Teamwork',
+    'Does not mention any collaborative projects or teamwork experiences.',
+    'Highlights various collaborative projects, demonstrating the ability to work effectively in a team.'
+), (
+    'Networking Expertise',
+    'Does not mention any experiences related to networking concepts or projects.',
+    'Provides detailed experiences and achievements in networking, showcasing in-depth knowledge.'
+), (
+    'Problem-Solving',
+    'Does not mention any experiences where problem-solving was essential.',
+    'Highlights various instances where problem-solving skills were crucial, providing detailed examples.'
+);
+
+INSERT INTO job_posting_categories (job_posting_id, category_id) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 4),
+(2, 5);
---
+++ src/services/evaluate_applicant.py
@@ -0,0 +1,217 @@
+import os
+import openai
+from dotenv import load_dotenv
+from langchain.llms import OpenAI as LangChainOpenAI
+import json
+import mysql.connector
+import time 
+
+class Applicant:
+    def __init__(self, first_name, last_name, phone_number, email_address, position):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email_address = email_address
+        self.position = position
+        self.ratings_cv = {}
+
+    def update_ratings(self, rating_data):
+        for category, details in rating_data.items():
+            self.ratings_cv[category] = {
+                "Score": details['Score'],
+                "Justification": details['Justification'],
+                "Quote": details['Quote'],
+            }
+
+def count_tokens(text):
+    # Teile den Text in Wörter und zähle die Token
+    words = text.split()
+    num_tokens = len(words)
+    return num_tokens
+
+def load_openai_model():
+    load_dotenv()
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    openai.api_key = OPENAI_API_KEY
+
+    class OpenAIModelIO(LangChainOpenAI):
+        def query(self, prompt):
+            truncated_prompt = prompt[:100] + "..." if len(prompt) > 100 else prompt
+            num_tokens = count_tokens(prompt)
+            print(f"Die Anfrage hat {num_tokens} Token.")
+            print("Prompt an OpenAI: ", {truncated_prompt} )
+
+            start_time = time.time()
+            try:
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system", "content": "You are a critical Human Resources professional who evaluates job applicants objectively and attentively."},
+                        {"role": "user", "content": prompt}
+                    ],
+                    timeout=100  # Set the timeout in seconds
+                )
+            except openai.error.OpenAIError as e:
+                print(f"Error from OpenAI: {e}")
+                return "Timeout: No response from OpenAI within the specified time."
+
+            end_time = time.time()
+
+            duration = end_time - start_time
+            print(f"Antwort von OpenAI erhalten (Dauer: {duration:.2f} Sekunden)")
+
+            return response.choices[0].message['content'].strip()
+
+    return OpenAIModelIO()
+
+def load_possible_positions(cursor):
+    cursor.execute("SELECT position FROM job_postings")
+    positions = [row[0] for row in cursor.fetchall()]
+    return positions
+
+def load_categories_for_position(cursor, position):
+    cursor.execute("""
+        SELECT jc.category_name, jc.guideline_0, jc.guideline_10
+        FROM job_categories jc
+        JOIN job_posting_categories jpc ON jc.id = jpc.category_id
+        JOIN job_postings jp ON jpc.job_posting_id = jp.id
+        WHERE jp.position = %s
+    """, (position,))
+    categories = [(row[0], row[1], row[2]) for row in cursor.fetchall()]
+    return categories
+
+def create_applicant(data, position):
+    return Applicant(data["First Name"], data["Last Name"], data["Phone Number"], data["Email Address"], position)
+
+def create_rating_prompt(categories, text, position):
+    rating_prompts = []
+    for category, guideline_0, guideline_10 in categories:
+        rating_prompts.append(
+            f'"{category}": {{"Score": "", "Justification": "", "Quote": "", "Guideline_0": "{guideline_0}", "Guideline_10": "{guideline_10}"}}'
+        )
+
+    categories_string = ", ".join(rating_prompts)
+    return f"""
+    Please rate the following categories from 0 to 10, and provide a justification and a quote from the application for each rating:
+    {text}
+
+    Ratings in JSON format:
+    {{
+        {categories_string}
+    }}
+    """
+
+def extract_applicant_metadata(openai_model, text):
+    prompt_cv_metadata = f"""
+    Extract metadata from the curriculum vitae:
+
+    {text}
+
+    Please provide the response in the following JSON format:
+    {{
+        "First Name": "",
+        "Last Name": "",
+        "Phone Number": "",
+        "Email Address": ""
+    }}
+    """
+    response_cv_metadata = openai_model.query(prompt_cv_metadata)
+    data = json.loads(response_cv_metadata)
+    return data
+
+def analyze_application(openai_model, text, position, cursor):
+    applicant_metadata = extract_applicant_metadata(openai_model, text)
+    applicant = create_applicant(applicant_metadata, position)
+
+    categories = load_categories_for_position(cursor, position)
+
+    rating_prompt = create_rating_prompt(categories, text, position)
+    rating_response = openai_model.query(rating_prompt)
+    rating_data = json.loads(rating_response)
+
+    applicant.update_ratings(rating_data)
+
+    return applicant
+
+def insert_applicant_and_ratings_into_database(applicant, cursor):
+    # Query to get position_id based on the provided position
+    get_position_id_query = "SELECT id FROM job_postings WHERE position = %s"
+    cursor.execute(get_position_id_query, (applicant.position,))
+    position_row = cursor.fetchone()
+
+    if position_row is not None:
+        position_id = position_row[0]
+
+        # Insert basic applicant information
+        insert_applicant_query = """
+        INSERT INTO applicants (first_name, last_name, phone_number, email_address, position_id)
+        VALUES (%s, %s, %s, %s, %s)
+        """
+        cursor.execute(insert_applicant_query, (applicant.first_name, applicant.last_name, applicant.phone_number, applicant.email_address, position_id))
+
+        # Get the last inserted applicant_id
+        applicant_id = cursor.lastrowid
+
+        # Insert ratings information
+        for category, details in applicant.ratings_cv.items():
+            insert_ratings_query = """
+            INSERT INTO applicant_ratings (applicant_id, category, score, justification, quote)
+            VALUES (%s, %s, %s, %s, %s)
+            """
+            cursor.execute(insert_ratings_query, (applicant_id, category, details['Score'], details['Justification'], details['Quote']))
+
+        # Commit the changes to the database
+        connection.commit()
+
+        # Print a summary of the applicant
+        print("Applicant Summary:")
+        print(f"Name: {applicant.first_name} {applicant.last_name}")
+        print(f"Phone Number: {applicant.phone_number}")
+        print(f"Email Address: {applicant.email_address}")
+        print(f"Position: {applicant.position}")
+
+        print("\nRatings:")
+        for category, details in applicant.ratings_cv.items():
+            print(f"{category}:")
+            print(f"  Score: {details['Score']}")
+            print(f"  Justification: {details['Justification']}")
+            print(f"  Quote: {details['Quote']}")
+
+    else:
+        print(f"Error: Position '{applicant.position}' not found in job_postings table.")
+
+
+# Connect to the database
+connection = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3307,
+    user="root",
+    password="password",
+    database="skillsync"
+)
+
+# Create a cursor
+cursor = connection.cursor()
+
+# Load possible positions from the database (für zukünftiges Automatisches Stellen erkennen anhand des Anschreibens)
+#possible_positions = load_possible_positions(cursor)
+# Specify the known position
+position = "java backend developer"
+
+# Pfad zur Datei
+file_path_cv = os.path.join(os.path.dirname(os.path.abspath(__file__)), "vector_input_data", "cv_en_martin_marketing.txt")
+
+# Überprüfen, ob die Datei existiert
+if os.path.exists(file_path_cv):
+    with open(file_path_cv, "r", encoding="utf-8") as file:
+        cv = file.read().replace('\n', '')
+
+    openai_model = load_openai_model()
+    applicant = analyze_application(openai_model, cv, position, cursor)
+    insert_applicant_and_ratings_into_database(applicant, cursor)
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+else:
+    print(f"Die Datei {file_path_cv} existiert nicht.")
