7d182778a09551b56c2766361cede028d3ee04df
Nicole Sauer <95630400+NicoleSauer@users.noreply.github.com>
2023-11-27 16:59:26 +0100

    added endpoint for file upload

---
+++ src/apis/applicant_api.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, request
+from flask_restful import Api, Resource
+import os
+
+# Creates a new blueprint
+file_upload = Blueprint('file_upload', __name__)
+api = Api(file_upload)
+
+
+# Class containing all endpoints for applicants
+class ApplicantResource(Resource):
+
+    def get(self):
+        return 'Hello Upload Page!'
+
+    def post(self):
+        if 'pdfFile' not in request.files:
+            return 'No file part found in POST request.', 400
+
+        pdf_file = request.files['pdfFile']
+
+        # Handle the uploaded file (save, process, etc.)
+        print('File name: ' + pdf_file.filename)
+
+        # Change filename to the applicant id
+        # TODO: Generate UUID for applicant id somewhere
+        # TODO: Use applicant id instead of 1234
+        pdf_file.filename = 'applicant_1234.pdf'
+
+        # Check if the folder exists, if not create it
+        if not os.path.exists('./src/apis/uploads'):
+            os.makedirs('./src/apis/uploads')
+
+        # Save pdf file to the uploads folder
+        pdf_file.save('./src/apis/uploads/' + pdf_file.filename)
+
+        return f"File '{pdf_file.filename}' uploaded successfully.", 200
+
+
+# Add the resource to the api
+api.add_resource(ApplicantResource, '/upload')
--- src/app.py
+++ src/app.py
@@ -1,17 +1,19 @@
-# application dependencies
 from flask import Flask
 from flask_cors import CORS
 
 # Imports of the resources
-from resources.demo_resource import demo_resource
+from apis.demo_resource import demo_resource
+from apis.applicant_api import file_upload
 
+# Creates a new flask app
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Registers the resources, which are needed.
 # The (Flask-Restful)-Resources in this case are blueprints, which contain the different rest apis
 app.register_blueprint(demo_resource)
+app.register_blueprint(file_upload)
 
-
+# Starts the flask app
 if __name__ == '__main__':
     app.run()
