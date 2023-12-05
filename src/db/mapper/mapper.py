9ff79b4a7fd04188a75d5feed7d22e826d1fce2c
Scott Palme <Scottpalme@gmail.com>
2023-12-05 21:15:05 +0100

    Added mapper and updated tables of applicant

---
+++ src/db/mapper/mapper.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+import mysql.connector
+
+
+class Mapper(ABC):
+
+    def __init__(self):
+        self._connection = None
+
+    def __enter__(self):
+        self._connection = mysql.connector.connect(
+            user='root',
+            password='password',
+            host='127.0.0.1',
+            database='skillsync',
+            port='3307',
+
+        )
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._connection.close()
+
+    @abstractmethod
+    def get_all(self):
+        pass
+
+    @abstractmethod
+    def get_by_id(self, id):
+        pass
+
+    @abstractmethod
+    def insert(self, object):
+        pass
+
+    @abstractmethod
+    def update_id(self, id):
+        pass
+
+    @abstractmethod
+    def delete_by_id(self, id):
+        pass
---
+++ src/db/mapper/mysql_mapper/applicant_mapper.py
@@ -0,0 +1,60 @@
+from db.mapper.mapper import Mapper
+from classes.applicant import Applicant
+from uuid import UUID
+
+class ApplicantMapper(Mapper):
+
+    def __int__(self):
+        super().__init__()
+
+    def get_all(self):
+        pass
+
+    def get_by_id(self, applicant_id: str):
+        pass
+
+    def get_by_email(self, applicant_email: str):
+        result = []
+        cursor = self._connection.cursor()
+        cursor.execute("SELECT * FROM applicant WHERE email='{}'".format(applicant_email))
+        tuples = cursor.fetchall()
+        try:
+            (id, first_name, last_name, date_of_birth, street, postal_code, city, email, phone_number) = tuples[0]
+            applicant = Applicant(first_name, last_name, date_of_birth, street, postal_code, city, email, phone_number)
+            applicant.set_id(UUID(id))
+            result = applicant
+
+        except IndexError:
+            result = None
+
+        self._connection.commit()
+        cursor.close()
+        return result
+
+    def insert(self, applicant: Applicant):
+        cursor = self._connection.cursor()
+        query = "INSERT INTO applicant (id, first_name, last_name, date_of_birth, street, postal_code, city, email, phone_number) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        data = (
+            str(applicant.get_id()),
+            applicant.get_first_name(),
+            applicant.get_last_name(),
+            applicant.get_date_of_birth(),
+            applicant.get_street(),
+            applicant.get_postal_code(),
+            applicant.get_city(),
+            applicant.get_email(),
+            applicant.get_phone_number()
+        )
+
+        cursor.execute(query, data)
+
+        self._connection.commit()
+        cursor.close()
+
+    def update_id(self, applicant_id: str):
+        pass
+
+    def delete_by_id(self, applicant_id: str):
+        pass
+
+
