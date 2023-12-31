c3b33dc027864977111b904a52ee6c48699275d5
Nicole Sauer <95630400+NicoleSauer@users.noreply.github.com>
2023-12-31 14:40:37 +0100

    fixed mistake from merging...

--- src/services/openai_service.py
+++ src/services/openai_service.py
@@ -1,7 +1,8 @@
 import openai
 import os
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 import time
+from services.log_service import log
 
 
 def load_dot_env() -> None:
@@ -24,6 +25,7 @@ def count_tokens(prompt) -> int:
     num_tokens = len(words)
     return num_tokens
 
+
 def execute_prompt(prompt):
     """
     Sends the prompt to the OpenAI model
@@ -32,21 +34,26 @@ def execute_prompt(prompt):
     """
     truncated_prompt = prompt[:100] + "..." if len(prompt) > 100 else prompt
     num_tokens = count_tokens(prompt)
-    print(f"Die Anfrage hat {num_tokens} Token.")
-    print("Prompt an OpenAI: ", {truncated_prompt})
+    # print(f"Die Anfrage hat {num_tokens} Token.")
+    # print("Prompt an OpenAI: ", {truncated_prompt})
+    print(f"Sending request ({num_tokens} tokens) to GPT-4...")
+    log("rating_service", "GPT-4 Request: " + prompt)
 
     start_time = time.time()
     try:
         response = openai.ChatCompletion.create(
             model="gpt-4-1106-preview",
             messages=[
-                {"role": "system",
-                 "content": "You are a critical Human Resources professional who evaluates job applicants objectively and attentively."},
-                {"role": "user", "content": prompt}
+                {
+                    "role": "system",
+                    "content": "You are a critical Human Resources professional who evaluates job applicants objectively and attentively.",
+                },
+                {"role": "user", "content": prompt},
             ],
             timeout=200,  # Set the timeout in seconds
-            n=1
+            n=1,
         )
+        log("rating_service", "GPT-4 Response: " + str(response))
     except openai.error.OpenAIError as e:
         print(f"Error from OpenAI: {e}")
         return "Timeout: No response from OpenAI within the specified time."
@@ -54,6 +61,7 @@ def execute_prompt(prompt):
     end_time = time.time()
 
     duration = end_time - start_time
-    print(f"Antwort von OpenAI erhalten (Dauer: {duration:.2f} Sekunden)")
-    print(response.choices[0].message['content'].strip())
-    return response.choices[0].message['content'].strip()
+    print(f"Response from GPT-4 received. It took {duration:.2f} seconds.")
+
+    response_string = response.choices[0].message["content"].strip()
+    return response_string
--- src/services/rating_service.py
+++ src/services/rating_service.py
@@ -1,6 +1,5 @@
 import os
 from typing import List
-import services.openai_service as openai_service
 import openai
 from dotenv import load_dotenv, find_dotenv
 import time
@@ -10,27 +9,8 @@ from uuid import UUID
 from classes.applicant import Applicant
 from classes.rating import Rating
 import json
-
-
-def load_dot_env() -> None:
-    """
-    Loads the api-key from the .env
-    :return: None
-    """
-    load_dotenv()
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    openai.api_key = OPENAI_API_KEY
-
-
-def count_tokens(prompt) -> int:
-    """
-    Returns the amount of tokens inside a prompt
-    :param prompt: Prompt which should be tokenized
-    :return: Amount of tokens
-    """
-    words = prompt.split()
-    num_tokens = len(words)
-    return num_tokens
+from services.log_service import log
+from services import openai_service
 
 
 def get_list_of_categories_from_vacancy(vacancy_id: UUID) -> List:
@@ -48,47 +28,6 @@ def get_list_of_categories_from_vacancy(vacancy_id: UUID) -> List:
         categories.append(category)
 
     return categories
-
-
-def execute_prompt(prompt):
-    """
-    Sends the prompt to the OpenAI model
-    :param prompt: Prompt which, should be sent to the model
-    :return: The response from the model
-    """
-    truncated_prompt = prompt[:100] + "..." if len(prompt) > 100 else prompt
-    num_tokens = count_tokens(prompt)
-    # print(f"Die Anfrage hat {num_tokens} Token.")
-    # print("Prompt an OpenAI: ", {truncated_prompt})
-    print(f"Sending request ({num_tokens} tokens) to GPT-4...")
-    log("rating_service", "GPT-4 Request: " + prompt)
-
-    start_time = time.time()
-    try:
-        response = openai.ChatCompletion.create(
-            model="gpt-4-1106-preview",
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You are a critical Human Resources professional who evaluates job applicants objectively and attentively.",
-                },
-                {"role": "user", "content": prompt},
-            ],
-            timeout=200,  # Set the timeout in seconds
-            n=1,
-        )
-        log("rating_service", "GPT-4 Response: " + str(response))
-    except openai.error.OpenAIError as e:
-        print(f"Error from OpenAI: {e}")
-        return "Timeout: No response from OpenAI within the specified time."
-
-    end_time = time.time()
-
-    duration = end_time - start_time
-    print(f"Response from GPT-4 received. It took {duration:.2f} seconds.")
-
-    response_string = response.choices[0].message["content"].strip()
-    return response_string
 
 
 def create_rating_prompt(categories, cv_content):
